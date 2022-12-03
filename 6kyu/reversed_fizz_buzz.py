829e694b287a82a46cd5c9dc28cd110a1c44873b
Vasil Angelov <vasilangelov83@gmail.com>
2022-12-03 22:38:59 +0100

    FizzBuzz is often one of the first programming puzzles people learn. Now undo it with reverse FizzBuzz!
    Write a function that accepts a string, which will always be a valid section of FizzBuzz. Your function must return an array that contains the numbers in order to generate the given section of FizzBuzz.
    
    Notes:
    
    If the sequence can appear multiple times within FizzBuzz, return the numbers that generate the first instance of that sequence.
    All numbers in the sequence will be greater than zero.
    You will never receive an empty sequence.
    Examples
    reverse_fizzbuzz("1 2 Fizz 4 Buzz")        -->  [1, 2, 3, 4, 5]
    reverse_fizzbuzz("Fizz 688 689 FizzBuzz")  -->  [687, 688, 689, 690]
    reverse_fizzbuzz("Fizz Buzz")              -->  [9, 10]

---
+++ 6kyu/reversed_fizz_buzz.py
@@ -0,0 +1,25 @@
+import sys
+def reverse_fizzbuzz(string):
+
+    list_of_num = [int(x) if x.isdigit() else x for x in string.split(" ")]
+
+    for x in list_of_num:
+        if type(x) is int:
+            result =[i[0] for i in list(enumerate(list_of_num,start=x - list_of_num.index(x)))]
+        elif list_of_num == ["Fizz","Buzz"]:
+            result = [9,10]
+        elif list_of_num == ['Buzz','Fizz']:
+            result = [5,6]
+        elif list_of_num == ['Fizz']:
+            result = [3]
+        elif list_of_num == ['Buzz']:
+            result = [5]
+        elif list_of_num == ['FizzBuzz']:
+            result = [15]
+
+
+
+    return  result
+print(reverse_fizzbuzz("1 2 Fizz 4 Buzz"))
+print(reverse_fizzbuzz("Fizz 688 689 FizzBuzz"))
+print(reverse_fizzbuzz("Fizz Buzz"))
